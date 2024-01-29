db517a960f90caa58d566a17fd7180e7d08b0a81
theSimpson <kevinnoa02@gmail.com>
2024-01-29 12:33:31 -0500

    primer proyecto terminado

---
+++ Proyectos Python/proyecto01.py
@@ -0,0 +1,7 @@
+import string
+import random
+longitud= int(input("Ingrese su logintud de contraseña:  "))
+caracteres= string.ascii_letters + string.digits + string.punctuation
+contraseña = "".join(random.choice(caracteres) for i in range(longitud))
+print(f"La contraseña generadas es   {contraseña}")
+
